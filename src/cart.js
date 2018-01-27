9ad8b277ee08d19b5f2a8984890b6fb185fe0fd3
Trevor Ford <info@trvrfrd.net>
2018-01-27 13:01:11 -0500

    BIG first pass at cart + tests

--- src/cart.js
+++ src/cart.js
@@ -2,14 +2,54 @@
 
 exports.createCart = createCart;
 
-function createCart(items = []) {
+function createCart(catalog) {
+  var _items = [];
+
+  function _getItemTotal(item) {
+    return item.quantity * item.price;
+  }
+
+  function _getItem(id) {
+    return _items.find(i => i.id === id);
+  }
+
+  function _checkItemProps(item) {
+    if (item.id == undefined) throw new Error('Item must have an id attribute to be added to cart');
+    if (item.price == undefined) throw new Error('Item must have a price attribute to be added to cart');
+  }
+
   return {
-    getItem(index) {
-      return items[index];
+    addItem(item) {
+      _checkItemProps(item);
+      // should price get updated each add? what if it has changed in Catalog?
+      item = _getItem(item.id) || { quantity: 0, id: item.id, price: item.price };
+      if (item.quantity === 0) _items.push(item);
+      item.quantity += 1;
     },
 
-    addItem(product) {
-      items.push(product);
+    updateQuantity(id, quantity) {
+      var item = _getItem(id);
+      if (item) item.quantity = quantity;
+    },
+
+    removeItem(id) {
+      var item = _getItem(id);
+      if (item) {
+        var index = _items.indexOf(item);
+        _items.splice(index, 1);
+      }
+    },
+
+    empty() {
+      _items = [];
+    },
+
+    getTotal() {
+      return _items.reduce((sum, item) => sum + _getItemTotal(item), 0);
+    },
+
+    map(fn) {
+      return _items.map(item => fn(Object.assign({}, item)));
     }
   }
 }
--- test/cart_test.js
+++ test/cart_test.js
@@ -1,29 +1,92 @@
 const assert = require('chai').assert;
 const createCart = require('../src/cart').createCart;
 
-const mockItems = () => [
-  {
-    id: 0
-  },
-  {
-    id: 1
-  },
-  {
-    id: 2
-  }
-];
+describe('Cart', () => {
+  var cart;
+  beforeEach(() => cart = createCart());
 
-describe("Cart", () => {
-  it("contains items", () => {
-    var items = mockItems();
-    var cart = createCart(items);
-    assert.deepEqual(cart.getItem(0), { id: 0 });
+  it('does not add an item without an id', () => {
+    var item = { price: 2.99 };
+
+    assert.throws(() => cart.addItem(item), /id/i);
   });
 
-  it("adds items", () => {
-    var cart = createCart();
-    cart.addItem({ id: 3 });
-    assert.deepEqual(cart.getItem(0), { id: 3 });
+  it('does not add an item without a price', () => {
+    var item = { id: 3 };
+
+    assert.throws(() => cart.addItem(item), /price/i);
+  });
+
+  it('adds an item', () => {
+    var item = { id: 3, price: 2.99 };
+
+    assert.doesNotThrow(() => cart.addItem(item));
+  });
+
+  it('can calculate the total price for the cart', () => {
+    var item1 = { id: 1, price: 2.99 };
+    var item2 = { id: 2, price: 5.99 };
+    cart.addItem(item1);
+    cart.addItem(item1);
+    cart.addItem(item2);
+
+    var expected = (2 * item1.price) + item2.price;
+    assert.equal(cart.getTotal(), expected);
+  });
+
+  it('returns a total of 0 for an empty cart', () => {
+    assert.equal(cart.getTotal(), 0);
+  });
+
+  it('can update the quantity of a given item', () => {
+    var item = { id: 3, price: 2.99 };
+    cart.addItem(item);
+    cart.updateQuantity(item.id, 5);
+
+    var expected = 5 * item.price;
+    assert.equal(cart.getTotal(), expected);
+  });
+
+  it('can remove an item', () => {
+    var item1 = { id: 1, price: 2.99 };
+    var item2 = { id: 2, price: 5.99 };
+    cart.addItem(item1);
+    cart.addItem(item2);
+    cart.removeItem(item1.id);
+
+    var expected = item2.price;
+    assert.equal(cart.getTotal(), expected);
+  });
+
+  it('can empty cart', () => {
+    var item1 = { id: 1, price: 2.99 };
+    var item2 = { id: 2, price: 5.99 };
+    cart.addItem(item1);
+    cart.addItem(item2);
+
+    cart.empty();
+    assert.equal(cart.getTotal(), 0);
+  });
+
+  it('can map over items', () => {
+    var item1 = { id: 1, price: 2.99 };
+    var item2 = { id: 2, price: 5.99 };
+    cart.addItem(item1);
+    cart.addItem(item2);
+
+    var expected = [2.99, 5.99];
+    var actual = cart.map(item => item.price);
+    assert.deepEqual(actual, expected);
+  });
+
+  it('cannot mutate items when mapping over them', () => {
+    var item1 = { id: 1, price: 2.99 };
+    var item2 = { id: 2, price: 5.99 };
+    cart.addItem(item1);
+    cart.addItem(item2);
+
+    cart.map(item => item.price = 0);
+    assert.notEqual(cart.getTotal(), 0);
   });
 });
 
