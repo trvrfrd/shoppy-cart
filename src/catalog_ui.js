1c7d7f3a77ec522c0802cb80fcfeef09d3e8f1cd
Trevor Ford <info@trvrfrd.net>
2018-02-21 19:13:46 -0500

    proper dynamic catalog rendering

--- src/catalog_ui.js
+++ src/catalog_ui.js
@@ -3,9 +3,40 @@
 exports.renderCatalog = renderCatalog;
 
 function renderCatalog(rootNode, catalog) {
-  var div = document.createElement('div');
-  div.textContent = 'rendered catalog!';
-  rootNode.appendChild(div);
+  var list = document.createElement('ul');
+
+  var listItems = catalog.map(renderProduct);
+  listItems.forEach(function(item) { list.appendChild(item) });
+
+  rootNode.appendChild(list);
+}
+
+function createElement(tagName, options) {
+  var element = document.createElement(tagName);
+  if (options.className) element.className = options.className;
+  if (options.text) element.textContent = options.text;
+  return element;
+}
+
+function renderProduct(product) {
+  var item = createElement('li', { className: 'product' });
+
+  var header = createElement('div', { className: 'product-header' });
+  header.appendChild(createElement('h3', { text: 'Chocolate Type' }));
+  header.appendChild(createElement('span', {
+    className: 'price',
+    text: '$' + parseFloat(product.price).toFixed(2)
+  }));
+  header.appendChild(createElement('button', { text: 'Add to Cart' }));
+
+  item.appendChild(header);
+
+  item.appendChild(createElement('p', {
+    className: 'product-description',
+    text: product.description
+  }));
+
+  return item;
 }
 
 }(typeof module !== 'undefined' ? module.exports : window));
